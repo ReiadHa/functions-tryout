1b07a010966e9881439d113b6e2ca65fdd19da60
Riad Hak <99054420@mydavinci.nl>
2021-10-27 11:28:44 +0200

    opdracht2

--- helloworld1.py
+++ helloworld1.py
@@ -1,10 +1,10 @@
-def my_function(x):
+
+def my_function(x=1): 
     x = x * ['Hello World!']
     for y in x:
-           print(y)
-                    
-
-    
-my_function(4)
+        print(y)
+   
+                
+my_function(1)
     
     
---
+++ helloworld2.py
@@ -0,0 +1,26 @@
+def plus(a,b):
+    return(a+b)
+
+    
+def min(a,b):
+    return(a-b)
+
+def multip(a,b):
+    return(a*b)
+
+def subt(a,b):
+    return(a/b)
+
+def plus1(a,b):
+    return(a+b)
+
+def min1(a,b):
+    return(a-b)
+ 
+  
+print(10, '  + ', 12, ' = ',(plus(10,12)))
+print(58, '  - ', 34, ' = ',(min(58,34)))
+print(6, '   x ', 7, '  = ' ,(multip(6,7)))
+print(144, ' ÷ ', 12, ' = ',int(subt(144,12)))
+print(12, '  + ', 1, '  = ',(plus1(12,1)))
+print(34, '  - ', 1, '  = ',(min1(34,1)))
