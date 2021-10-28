8314f7f02ba247ecef4c4aedf53c4a40da5eb44d
unknown <99054420@mydavinci.nl>
2021-10-28 06:43:02 -0700

    update

---
+++ tryout.py
@@ -0,0 +1,15 @@
+# def printAsText(anyType):
+#     print(str(anyType))
+
+# def showNumberTotal(maxNumber, prevNumber=0, total=0):
+#     number = prevNumber + 1
+#     total += number
+
+
+#     if (maxNumber == number):
+#         printAsText(number)
+#     else:
+#         showNumberTotal(maxNumber, number, total)
+
+# showNumberTotal(6)
+print('asshole\naashole2')
